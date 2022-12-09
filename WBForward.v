0374e70b4bf5010632596f58887c6d7519efa48b
Mkeshishian <89661528+Mkeshishian@users.noreply.github.com>
2022-12-08 23:56:38 -0800

    added comment

--- WBForward.v
+++ WBForward.v
@@ -1,5 +1,4 @@
 `timescale 1 ps / 100 fs
-// fpga4student.com: FPGA projects, Verilog Projects, VHDL projects
 // Verilog project: 32-bit 5-stage Pipelined MIPS Processor in Verilog 
 // Write Back Forwarding 
 module WB_forward(ReadData1Out,ReadData2Out,ReadData1,ReadData2,rs,rt,WriteRegister,WriteData,RegWrite);
