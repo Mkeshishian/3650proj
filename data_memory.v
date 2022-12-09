9a9d5c2df12b903ea06f5a1f7dfe306f773aa99a
Mkeshishian <89661528+Mkeshishian@users.noreply.github.com>
2022-12-08 23:57:16 -0800

    added comment

--- data_memory.v
+++ data_memory.v
@@ -1,4 +1,3 @@
-//fpga4student.com: FPGA projects, Verilog projects, VHDL projects
 // Verilog project: Verilog code for 16-bit MIPS Processor
 // Submodule: Data memory in Verilog 
  module data_memory  
