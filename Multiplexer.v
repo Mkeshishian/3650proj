14c1a6bff3b6b8260d67e62fe7a1a0b902a89177
Mkeshishian <89661528+Mkeshishian@users.noreply.github.com>
2022-12-08 23:53:53 -0800

    added comment

--- Multiplexer.v
+++ Multiplexer.v
@@ -1,5 +1,4 @@
 `timescale 1 ps / 100 fs
-// fpga4student.com: FPGA projects, Verilog Projects, VHDL projects
 // Verilog project: 32-bit 5-stage Pipelined MIPS Processor in Verilog 
 // mux2x32to32
 module mux2x32to32( DataOut,Data0, Data1, Select);
